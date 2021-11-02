d0ba682ee18d0c7d23412d08104c2e7c84f441ca
Jochem Baas <jochembaas3@gmail.com>
2021-11-02 13:46:21 +0100

    Added function for measuring speed

--- receivingPC/receivingPC.py
+++ receivingPC/receivingPC.py
@@ -2,23 +2,32 @@ import warnings
 import serial
 import serial.tools.list_ports
 from serial import Serial
+import time
 
+DATARATE = 0.25
+currentTime = time.time()
+previousTime = time.time()
 
-sPort = '/dev/cu.usbmodem14101'
+sPort = '/dev/cu.usbmodem14201'
 
 aSerialData = serial.Serial(sPort,115201)
 
 receiving = True
 
+bitcount = 0
 
 # main loop
 while receiving:
-    # if serial data is found
+    currentTime = time.time()
+    if(currentTime - previousTime > DATARATE):
+        previousTime = currentTime
+        print( 20 * "\n", bitcount *4, "b/s")
+        bitcount = 0
+
     if (aSerialData.inWaiting()>0):
 
-        # get bits
         sData = aSerialData.readline()
         bit = str(sData)[2]
-        print(bit)
+        bitcount+=1
 
         
