48ae551169ce98456deee860de47e98d4c12cf3b
Jochem Baas <jochembaas3@gmail.com>
2021-11-10 09:21:39 +0100

    Sending manchester now works (almost every time)

--- sendingARDUINO/sendingManchester/sendingManchester.ino
+++ sendingARDUINO/sendingManchester/sendingManchester.ino
@@ -16,8 +16,7 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     receivedBit = Serial.read();
-    Serial.println(receivedBit);
-//    Serial.print(",");
+    Serial.println(ledState);
     received = true;
     ledState = receivedBit - 48;
     
@@ -25,55 +24,8 @@ void loop() {
     previousMicros = micros();
   }
 
-  if(previousMicros - micros() < halfBitTime){
-     Serial.println(halfBitTime);
-     digitalWrite(ledPin, ledState * -1 + 1);
+  if(micros() - previousMicros > halfBitTime){
+    Serial.println(ledState);
+    digitalWrite(ledPin, ledState * -1 + 1);
   }
 }
-
-
-
-
-
-
-
-//
-//
-//unsigned long bitTime = 100000;
-//unsigned long halfBitTime = bitTime / 2;
-//unsigned long previousMillis = 0;
-//const int ledPin =  LED_BUILTIN;
-//
-//int receivedBit = 0;
-//int ledState = 0;
-//
-//bool received = false;
-//
-//void setup() {
-//  pinMode(ledPin, OUTPUT);
-//  Serial.begin(115201);
-//}
-//
-//void loop() {
-//  if (Serial.available() > 0) {
-//    receivedBit = Serial.read();
-//    Serial.println(receivedBit);
-//    received = true;
-//  }
-//  
-//  unsigned long currentMillis = micros();
-//  if (currentMillis - previousMillis >= bitTime) {
-//    received = false;
-//    previousMillis = currentMillis;
-//    ledState = receivedBit - 48;
-//    digitalWrite(ledPin, ledState);
-//  }
-//
-//  if (currentMillis - previousMillis >= halfBitTime && received) {
-//    if(ledState == 1){
-//       digitalWrite(ledPin, 0);
-//     } else {
-//       digitalWrite(ledPin, 1);
-//     }
-//  }
-//}
