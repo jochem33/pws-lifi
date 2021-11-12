4d64965853c625575d86e98bd17350d3e07a75be
Jochem Baas <jochembaas3@gmail.com>
2021-11-12 16:16:50 +0100

    Receiver now succesfully reads frames and decodes into text

--- receivingPC/receivingPC.py
+++ receivingPC/receivingPC.py
@@ -5,41 +5,21 @@ import serial.tools.list_ports
 from serial import Serial
 import time
 
+FRAMESIZE = 64
 DATARATE = 0.24
 currentTime = time.time()
 previousTime = time.time()
 
-FRAMASTART = [0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,1]
-ANTIFRAMASTART = [1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,0]
+FRAMASTART = ["0","0","0","0","0","0","0","0", "0","0","0","0","0","0","0","1"]
+ANTIFRAMASTART = ["1","1","1","1","1","1","1","1", "1","1","1","1","1","1","1","0"]
 
 
 sPort = '/dev/cu.usbmodem14101'
 
 aSerialData = serial.Serial(sPort,115201)
 
-receiving = True
+receiving = False
 
-bitcount = 0
-
-# main loop
-while receiving:
-    currentTime = time.time()
-    if(currentTime - previousTime > DATARATE):
-        previousTime = currentTime
-        print(" ", bitcount * (1/DATARATE), "b/s")
-        bitcount = 0
-
-    if (aSerialData.inWaiting()>0):
-
-        sData = aSerialData.readline()
-        bit = str(sData)[2]
-        char = ""
-        if(bit == "0"):
-            char = "_"
-        elif(bit == "1"):
-            char = "▓"
-        print(char, end='')
-        bitcount+=1
 
         
 def findFrameStart():
@@ -51,7 +31,42 @@ def findFrameStart():
             sData = aSerialData.readline()
             bit = str(sData)[2]
             syncList.append(bit)
-            if(syncList):
-                print("")
+            if(syncList == FRAMASTART):
+                print("Found, normal")
+                return False
+            elif(syncList == ANTIFRAMASTART):
+                print("Found, flipped")
+                return True
+
+        
+def readFrame(flipped):
+    payload = ""
+    while len(payload) < FRAMESIZE:
+        # currentTime = time.time()
+        # if(currentTime - previousTime > DATARATE):
+        #     previousTime = currentTime
+        #     print(" ", bitcount * (1/DATARATE), "b/s")
+        #     bitcount = 0
+
+        if (aSerialData.inWaiting()>0):
+
+            sData = aSerialData.readline()
+            bit = str(sData)[2]
+            if(flipped):
+                bit = int(bit) * -1 + 1
+            payload = payload + str(bit)
+    return payload
+
+
+def decode_binary_string(s):
+    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
+
+while True:
+    flipped = findFrameStart()
+
+    print((readFrame(flipped)))
+
+    print(decode_binary_string(readFrame(flipped)))
+
 
 
