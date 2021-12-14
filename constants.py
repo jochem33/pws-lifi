e46eb64262b4b87e8e996306e78d118fdbcc7661
Jochem Baas <jochembaas3@gmail.com>
2021-12-14 13:29:13 +0100

    Added extra results, last commit before bidirectional

--- constants.py
+++ constants.py
@@ -19,8 +19,8 @@ TESTTIME = 600
 FRAMESTART = ["0","0","0","0","0","0","0","0", "0","0","0","0","0","0","0","1"]
 ANTIFRAMESTART = ["1","1","1","1","1","1","1","1", "1","1","1","1","1","1","1","0"]
 
-RECEIVINGDEVICE = '/dev/cu.usbmodem14101'
-SENDINGDEVICE = '/dev/cu.usbserial-1420'
+RECEIVINGDEVICE = '/dev/cu.usbmodem14201'
+SENDINGDEVICE = '/dev/cu.usbserial-1410'
 
 INPUTFILE = 'input.html'
 OUTPUTFILE = 'output.html'
