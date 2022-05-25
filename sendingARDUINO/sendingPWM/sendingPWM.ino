19f77a81cda1f1539365d76f74b1da56eb0bb587
Jochem Baas <jochembaas3@gmail.com>
2022-05-25 18:18:10 +0200

    Added testcode using servo library

--- sendingARDUINO/sendingPWM/sendingPWM.ino
+++ sendingARDUINO/sendingPWM/sendingPWM.ino
@@ -37,3 +37,44 @@ void loop() {
 
   }
 }
+
+
+
+//
+//#include <Servo.h>
+//
+//Servo laser;
+//
+//const unsigned long symbolLenght = 1000;
+//const unsigned long endSymbolGap = 400;
+//const int symbolCount = 16;
+//const long capCorrection = 0;
+//const unsigned long fragmentLenght = (symbolLenght - endSymbolGap) / symbolCount - capCorrection;
+//
+//unsigned long ontime;
+//
+//unsigned long previousMicros = 0;
+//const int ledPin =  LED_BUILTIN;
+//
+//int receivedSymbol = 0;
+//int ledState = 0;
+//
+//
+//void setup() {
+//  laser.attach(5);
+//  Serial.begin(250000);
+//}
+//
+//void loop() {
+//  if (Serial.available() > 0) {
+//    receivedSymbol = Serial.read() + 1;
+////    ontime = receivedSymbol * fragmentLenght;
+//
+//    laser.writeMicroseconds(receivedSymbol * fragmentLenght);
+//  }
+//
+////  if(micros() - previousMicros > ontime){
+////    PORTD = B00000000;
+////
+////  }
+//}
